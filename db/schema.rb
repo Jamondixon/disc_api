437d7e720a05580a2568801cfdf65b24f236640c
Jamon Dixon <jamonadixon@gmail.com>
2021-09-11 17:29:01 -0500

    pg server

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_01_202654) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "discs", force: :cascade do |t|
     t.string "maker"
     t.string "model"
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_202654) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.integer "disc_id", null: false
+    t.bigint "disc_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["disc_id"], name: "index_users_on_disc_id"
